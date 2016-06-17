bbfecd84e1662a4edc1f74c16501ab487554a7db
Rolando Vargas <rolovargas@gmail.com>
2016-06-16 19:36:00 -0700

    resurrect task

--- emberFrontEnd/app/controllers/taskscomplete.js
+++ emberFrontEnd/app/controllers/taskscomplete.js
@@ -15,22 +15,44 @@ export default Ember.Controller.extend({
 			this.send('reloadModel');
 		},
 
-	deleteAllTasks: function(){
-    var currentController = this;
-    var urlCall = "http://localhost:3000/status/" + currentController.taskMode;
-		// console.log(urlCall);
-		// delete the tasks from mongo
-		Ember.$.ajax({
-			type: "DELETE",
-			url: urlCall
-			}).then(function(){
-				currentController.send('refreshModel');
-			});
+		deleteAllTasks: function(){
+		var currentController = this;
+		var urlCall = "http://localhost:3000/status/" + currentController.taskMode;
+			// console.log(urlCall);
+			// delete the tasks from mongo
+			Ember.$.ajax({
+				type: "DELETE",
+				url: urlCall
+				}).then(function(){
+					currentController.send('refreshModel');
+				});
 		},
 
 	setTaskMode: function(taskMode){
 		var currentController = this;
 		currentController.set('taskMode', taskMode);
+		},
+
+	resurrectTask: function(task){
+			var currentController = this;
+			// console.log('task:' + JSON.stringify(task));
+			var reqBody = {};
+			reqBody.status = 'pending';
+			var taskId = task._id;
+			var url = "http://localhost:3000/task/" + taskId;
+			// console.log(url);
+
+			// call the mongo service to update the task as complete
+			Ember.$.ajax({
+				type: "POST",
+				url: url,
+				data: reqBody
+				}).then(function(){
+				// }).then(function(response){
+					// console.log(response);
+					// refresh the list
+					currentController.send('refreshModel');
+					});
 		}
 
 	}
