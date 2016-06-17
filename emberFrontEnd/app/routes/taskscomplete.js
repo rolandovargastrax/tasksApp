fba384a325c59d764b575d027a68e0f7188c2fb3
Rolando Vargas <rolovargas@gmail.com>
2016-06-16 17:22:51 -0700

    update sorting to match new backend behavior

--- emberFrontEnd/app/routes/taskscomplete.js
+++ emberFrontEnd/app/routes/taskscomplete.js
@@ -1,30 +1,12 @@
-// import Ember from 'ember';
-
-// export default Ember.Route.extend({
-// 	model: function() {
-// 		return Ember.$.getJSON('http://localhost:3000/status/complete').then(function(response){
-// 			// display the results to the console (for debugging purposes)
-// 			// console.log(JSON.stringify(response));
-// 			return response;
-// 		});
-// 	},
-
-// 	actions: {
-// 		reloadModel: function() {
-// 			this.refresh();
-// 		}
-// 	}
-// });
-
 
 import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {
 		var results = {};
-		return Ember.$.getJSON('http://localhost:3000/status/complete/completeDate').then(function(response){
+		return Ember.$.getJSON('http://localhost:3000/status/complete/completeDate/-1').then(function(response){
 			results.completeTasks = response;
-			return Ember.$.getJSON('http://localhost:3000/status/cancelled/completeDate').then(function(response){
+			return Ember.$.getJSON('http://localhost:3000/status/cancelled/completeDate/-1').then(function(response){
 				results.cancelledTasks = response;
 				return results;
 			});
