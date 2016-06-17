fd88d33f5470939135971455fabfb8b806c7c672
Rolando Vargas <rolovargas@gmail.com>
2016-06-16 17:16:25 -0700

    adding dynamic sort and get override

--- expressBackend/routes/index.js
+++ expressBackend/routes/index.js
@@ -6,50 +6,70 @@ var assert = require('assert')
 var ObjectId = require('mongodb').ObjectId
 var url = 'mongodb://localhost:27017/test'
 
-// filters the tasks by status
-router.get('/status/:status/:sortField', function(req, res, next) {
-	// connect to the mongo database
-
+var getTasksImplementation = function(res, status, sortField, direction){
+	// connect to mongo database
 	MongoClient.connect(url, function(err, db){
 		if(db == null){
 			res.status(500).send("Error connecting to database");
 			return;
 		}
-		// assert.equal(null, err);
-		// console.log('Connected correctly to server.');
-		// call the find method
-		// for some reason it didn't like it when I had the line like  .sort({req.params.sortField: -1});
-		if(req.params.sortField == "creationDate"){
-			cursor = db.collection('tasks').find({"status": req.params.status}).sort({"creationDate": -1});
+		if(direction === null){
+			direction = 1;
 		}
-		else{
-			cursor = db.collection('tasks').find({"status": req.params.status}).sort({"completeDate": -1});
+		if(sortField === null){
+			sortField = "creationDate";
 		}
+		sortJson = {}
+		sortJson[sortField] = parseInt(direction);
+
+		cursor = db.collection('tasks').find({"status": status}, {sort: sortJson});
+
 		cursor.toArray( function(err, items){
 			// console.log('Tasks converted to array successfully');
 			db.close();
-			// update each task to expose the date parts
-			for (var currentItem = items.length - 1; currentItem >= 0; currentItem--) {
-				// populate the formatted creationDate
-				if(items[currentItem].creationDate != null){
-					try{
-						items[currentItem].creationDateStr = items[currentItem].creationDate.getFullYear() + '.' + ('00' + (items[currentItem].creationDate.getMonth() + 1)).slice(-2) + '.' + ('00' + items[currentItem].creationDate.getDate()).slice(-2);
-					}catch(ex){;}
+			if( items != null ){
+				// update each task to expose the date parts
+				for (var currentItem = items.length - 1; currentItem >= 0; currentItem--) {
+					// populate the formatted creationDate
+					if(items[currentItem].creationDate != null){
+						try{
+							items[currentItem].creationDateStr = items[currentItem].creationDate.getFullYear() + '.' + ('00' + (items[currentItem].creationDate.getMonth() + 1)).slice(-2) + '.' + ('00' + items[currentItem].creationDate.getDate()).slice(-2);
+						}catch(ex){;}
+					}
+					// populate the formatted completeDate
+					if(items[currentItem].completeDate != null){
+						try{
+							items[currentItem].completeDateStr = items[currentItem].completeDate.getFullYear() + '.' + ('00' + (items[currentItem].completeDate.getMonth() + 1)).slice(-2) + '.' + ('00' + items[currentItem].completeDate.getDate()).slice(-2);
+						}catch(ex){;}
+					}
 				}
-				// populate the formatted completeDate
-				if(items[currentItem].completeDate != null){
-					try{
-						items[currentItem].completeDateStr = items[currentItem].completeDate.getFullYear() + '.' + ('00' + (items[currentItem].completeDate.getMonth() + 1)).slice(-2) + '.' + ('00' + items[currentItem].completeDate.getDate()).slice(-2);
-					}catch(ex){;}
-				}
+				// prepare the results to be returned
+				result = {}
+				result.items = items
+				res.json(result);
 			}
-			// prepare the results to be returned
-			result = {}
-			result.items = items
-			res.json(result);
+			else{
+				result = {}
+				result.items = [];
+				res.json(result);
+			}
 		});
 	});
+};
+
+
+// filters the tasks by status
+router.get('/status/:status', function(req, res, next) {
+	getTasksImplementation(res, req.params.status, "name", 1);
 });
+router.get('/status/:status/:sortField', function(req, res, next) {
+	getTasksImplementation(res, req.params.status, req.params.sortField, 1);
+});
+router.get('/status/:status/:sortField/:direction', function(req, res, next) {
+	getTasksImplementation(res, req.params.status, req.params.sortField, req.params.direction);
+});
+
+
 
 // delete all the tasks for a given status
 router.delete('/status/:status', function(req, res, next) {
